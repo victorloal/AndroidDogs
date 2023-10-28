795c8879f2bddf96d81d752f039a25db582e970c
Victor Lopez <victorloal@udenar.edu.co>
2023-10-27 23:27:23 -0500

    nuevo main - victor

--- app/src/main/java/com/example/dogs/MainActivity.kt
+++ app/src/main/java/com/example/dogs/MainActivity.kt
@@ -1,40 +1,51 @@
 package com.example.dogs
 
+import android.annotation.SuppressLint
 import android.app.Dialog
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import androidx.cardview.widget.CardView
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btRegister = findViewById<Button>(R.id.btRegister)
-        val btLogin = findViewById<Button>(R.id.btLogin)
+        val cardView = findViewById<CardView>(R.id.cvUser)
         val btInformation = findViewById<ImageButton>(R.id.btInformation)
 
-        btLogin.setOnClickListener{
-            showDialogLogin()
-        }
-        btRegister.setOnClickListener{
-            showDialogRegister()
-        }
+        val ibUser = findViewById<ImageButton>(R.id.ibUser)
+
+        cardView.setCardBackgroundColor(Color.TRANSPARENT)
+        cardView.setBackgroundColor(Color.TRANSPARENT)
         btInformation.setOnClickListener{
             showDialogInformation()
+        }
+        ibUser.setOnClickListener{
+            showDialogLogin()
         }
 
 
     }
+
+
+
     private fun showDialogLogin() {
         val dialog = Dialog(this)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(true)
+
+
         dialog.setContentView(R.layout.activity_user_login)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         val width = resources.displayMetrics.widthPixels
@@ -46,12 +57,22 @@ class MainActivity : AppCompatActivity() {
 
         val analytics = FirebaseAnalytics.getInstance(this)
         val bundle = Bundle()
+
+        val editTextUsername =dialog.findViewById<EditText>(R.id.editTextUsername)
+        val editTextPassword =dialog.findViewById<EditText>(R.id.editTextPassword)
         bundle.putString("message","Integración de Firebase completa")
         analytics.logEvent("InitScreen", bundle)
 
         btLogin.setOnClickListener {
+            if (editTextUsername.text.isNotEmpty() && editTextPassword.text.isNotEmpty() ){
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(editTextUsername.text.toString(),
+                        editTextPassword.text.toString()).addOnCompleteListener {
 
+                    }
+            }
         }
+
         dialog.show()
     }
 
--- app/src/main/java/com/example/dogs/UserLogin.kt
+++ app/src/main/java/com/example/dogs/UserLogin.kt
@@ -1,11 +1,41 @@
 package com.example.dogs
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class UserLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
+    }
+    private fun showDialogLogin() {
+        val btLogin =  findViewById<Button>(R.id.btLogin)
+
+
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+
+        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
+        val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+        bundle.putString("message","Integración de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+
+        btLogin.setOnClickListener {
+            if (editTextUsername.text.isNotEmpty() && editTextPassword.text.isNotEmpty() ){
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(editTextUsername.text.toString(),
+                        editTextPassword.text.toString()).addOnCompleteListener {
+
+                    }
+            }
+        }
     }
 }
